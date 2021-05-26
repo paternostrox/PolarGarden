7c3db19a3afa4e5f21ea8f2fce2767da3a267402
Paternostro <pedro.paternostro@gmail.com>
2021-05-26 00:58:46 -0300

    Stalk mesh generation WIP

--- source/ProcGeoTest.gd
+++ source/ProcGeoTest.gd
@@ -1,11 +1,59 @@
 extends MeshInstance
 
+var tube_radius = .1
+var tube_height = 1
+var tube_points = 3
+
+
 func _ready():
 	draw_plane(-1,0)
 	draw_plane(0.1,0)
 
 func draw_tube(p0, p1):
-	print("Drawing tube!")
+	var arr = []
+	arr.resize(Mesh.ARRAY_MAX)
+
+	# PoolVectorXXArrays for mesh construction.
+	var verts = PoolVector3Array()
+	var uvs = PoolVector2Array()
+	var normals = PoolVector3Array()
+	var indices = PoolIntArray()
+
+	## GENERATE MESH ##
+	var basis = Basis()
+
+	basis.y = p0-p1
+	basis.z = basis.y.cross(Vector3.RIGHT)
+	basis.x = basis.z.cross(basis.y)
+
+	# apply basis here?
+
+	var angle_inc = 360.0 / tube_points
+
+	var current_angle = 0
+	var current_point
+	var last_point
+	for  i  in range(tube_points):
+		current_point = Vector3(
+			rad2deg(cos(current_angle))*tube_radius,
+			0,
+			rad2deg(sin(current_angle))*tube_radius)
+
+		# left here
+
+		current_angle = (current_angle + angle_inc) % 360.0
+		last_point = current_point
+
+	# Assign arrays to mesh array.
+	arr[Mesh.ARRAY_VERTEX] = verts
+	arr[Mesh.ARRAY_TEX_UV] = uvs
+	arr[Mesh.ARRAY_NORMAL] = normals
+	arr[Mesh.ARRAY_INDEX] = indices
+
+	# Create mesh surface from mesh array.
+	mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, arr) # No blendshapes or compression used.
+
+
 
 # Draws plane from bottom left
 func draw_plane(x, z):
@@ -18,10 +66,7 @@ func draw_plane(x, z):
 	var normals = PoolVector3Array()
 	var indices = PoolIntArray()
 
-	#######################################
-	## Insert code here to generate mesh ##
-	#######################################
-	
+	## Generate mesh ##
 	verts.append(Vector3(x,0,z))
 	uvs.append(Vector2(0,0))
 	normals.append(Vector3(0,1,0))
